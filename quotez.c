1b6f25f63dc1e071e5edb940236d30a0b590189a
pseudoc <atlas.yu@canonical.com>
2022-09-13 09:43:19 +0800

    args support

--- quotez.c
+++ quotez.c
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 #include <json-c/json.h>
 
-int main(void) {
+void print_quote(array_list* quotes, size_t i) {
+  size_t n_quotes = array_list_length(quotes);
+  json_object* item = array_list_get_idx(quotes, i % n_quotes);
+  json_object* tobj = json_object_object_get(item, "text");
+
+  printf("%s\n", json_object_get_string(tobj));
+}
+
+int main(int argc, const char* args[]) {
   json_object* root = json_object_from_file("quotes.json");;
   array_list* quotes = json_object_get_array(root);
 
-  size_t n_quotes = array_list_length(quotes);
-  for (size_t i = 0; i < n_quotes; i++) {
-    json_object* item = array_list_get_idx(quotes, i);
-    json_object* tobj = json_object_object_get(item, "text");
-
-    printf("%s\n", json_object_get_string(tobj));
+  if (argc > 1) {
+    int index;
+    for (int i = 1; i < argc; i++)
+      print_quote(quotes, atoi(args[i]));
+    return 0;
   }
+
+  size_t n_quotes = array_list_length(quotes);
+  for (size_t i = 0; i < n_quotes; i++)
+    print_quote(quotes, i);
+
+  return 0;
 }
